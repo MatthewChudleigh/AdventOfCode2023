beeecedb6800ecd2375a54ff575dbd86aaacb7b7
Matthew Chudleigh <mchudleigh@sp.com.au>
2023-12-02 10:54:27 +1000

    Advent of Code Day 1 Part 2

--- day1/day1.exs
+++ day1/day1.exs
@@ -22,6 +22,7 @@ defmodule FileProcessor do
         end
       end)
 
+    IO.puts("Result:")
     case result do
       %{total: total} -> IO.puts(total)
       other -> IO.inspect(other)
@@ -32,11 +33,10 @@ defmodule FileProcessor do
   end
 
   defp sum_total(result) do
+    # IO.inspect(result)
     case result do
-      {:ok, _, %{head: head, tail: tail}} ->
+      {:ok, %{head: head, tail: tail}} ->
         {:ok, head*10 + tail}
-      {:ok, list, _} ->
-        {:error, "Missing numbers in #{list}"}
       other ->
         other
     end
@@ -45,54 +45,116 @@ defmodule FileProcessor do
   defp process_line(line) do
     # IO.puts("Line: #{line}")
     list = String.graphemes(line)
-    iterate_list(list, 0, length(list) - 1, %{})
+    iterate_list(list, 0, length(list) - 1, %{head_part: "", tail_part: ""})
   end
 
-  defp iterate_list(list, head, tail, numbers) when head <= tail and Kernel.map_size(numbers) < 2 do
-    # list, head)}, Tail: #{Enum.at(list, tail)}")
+  defp iterate_list(_, _, _, %{head: head, tail: tail}) do
+    {:ok, %{head: head, tail: tail}}
+  end
 
-    {head, numbers} = case try_map_number(list, :head, head, numbers) do
+  defp iterate_list(list, head, tail, numbers) when tail >= 0 and head < length(list) do
+    #  IO.puts("Head: #{Enum.at(list, head)}, Tail: #{Enum.at(list, tail)}")
+
+    # IO.puts("Head:")
+    {head, numbers} = case try_map_number(list, :head, numbers[:head_part], head, numbers) do
       {:ok, numbers} -> {head, numbers}
+      {:partial, part} -> {head+1, Map.put(numbers, :head_part, part)}
       _ -> {head+1, numbers}
     end
 
-    {tail, numbers} = case try_map_number(list, :tail, tail, numbers) do
+    # IO.puts("Tail:")
+    {tail, numbers} = case try_map_number(list, :tail, numbers[:tail_part], tail, numbers) do
       {:ok, numbers} -> {tail, numbers}
+      {:partial, part} -> {tail-1, Map.put(numbers, :tail_part, part)}
       _ -> {tail-1, numbers}
     end
 
     iterate_list(list, head, tail, numbers)
   end
 
-  defp iterate_list(list, head, tail, numbers) when head >= tail and length(numbers) < 2 do
-    {:error, "Could not find numbers in #{list}"}
-  end
-
   defp iterate_list(list, _, _, numbers) do
-    {:ok, list, numbers}
+    {:error, "Could not find numbers in #{list}", numbers}
   end
 
-  defp try_map_number(list, id, index, numbers) do
+  defp try_map_number(list, id, part, index, numbers) do
     case numbers[id] do
       nil ->
-        case try_find_number(list, index) do
+        case try_find_number(list, part, index) do
           {:ok, number} -> {:ok, Map.put(numbers, id, number)}
-          other -> other
+          {:partial, part} -> {:partial, part}
+          {:error, error} -> {:error, error}
         end
       _ -> {:ok, numbers}
     end
   end
 
-  defp try_find_number(list, index) do
-    case Enum.at(list, index) do
-      nil -> {:error, "Invalid Index"}
-      element ->
-        case Integer.parse(element) do
-          {number, ""} -> {:ok, number}
-          _ -> {:error, "Not a number"}
+  defp try_find_number(list, part, index) do
+    with {:ok, element} <- safe_element_at(list, index),
+         {:ok, number} <- parse_number_or_concat(part, element) do
+      {:ok, number}
+    else
+      :error -> {:error, "Invalid Index"}
+      {:partial, partial} -> {:partial, partial}
+    end
+  end
+
+  defp safe_element_at(list, index), do: Enum.fetch(list, index)
+
+  defp parse_number_or_concat(part, element) do
+    case Integer.parse(element) do
+      {number, ""} -> {:ok, number}
+      _ -> try_find_number_word(part, element)
+    end
+  end
+
+  # Placeholder for the try_find_number_word function
+  defp try_find_number_word(part, element) do
+    case try_find_number_word(part <> element) do
+      {:ok, number} -> {:ok, number}
+      {:partial, word} -> {:partial, word}
+      :no_match -> {:partial, element}
+    end
+  end
+
+  defp try_find_number_word(word) do
+    # IO.puts("\t#{word}")
+
+    number_words = [
+      "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+      "eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin"
+    ]
+
+    case word do
+      "one" -> {:ok, 1}
+      "two" -> {:ok, 2}
+      "three" -> {:ok, 3}
+      "four" -> {:ok, 4}
+      "five" -> {:ok, 5}
+      "six" -> {:ok, 6}
+      "seven" -> {:ok, 7}
+      "eight" -> {:ok, 8}
+      "nine" -> {:ok, 9}
+      "eno" -> {:ok, 1}
+      "owt" -> {:ok, 2}
+      "eerht" -> {:ok, 3}
+      "ruof" -> {:ok, 4}
+      "evif" -> {:ok, 5}
+      "xis" -> {:ok, 6}
+      "neves" -> {:ok, 7}
+      "thgie" -> {:ok, 8}
+      "enin" -> {:ok, 9}
+      _ ->
+        if Enum.any?(number_words, fn number_word -> String.starts_with?(number_word, word) end) do
+          {:partial, word}
+        else
+          case String.length(word) do
+            1 -> :no_match
+            length -> try_find_number_word(String.slice(word, 1, length - 1))
+          end
         end
     end
   end
+
 end
 
 case Enum.at(System.argv(), 0) do
